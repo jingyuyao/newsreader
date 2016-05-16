3338315f15ab15813045b5ff4a7e6727328ca8bd
Jingyu Yao <yjy.emails@gmail.com>
2016-05-15 23:46:02 -0400

    Basic layout + fixed sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var sass = require('gulp-sass');
 var gutil = require('gulp-util');
 var del = require('del');
 
 var paths = {
     buildRoot: './build/',
     coffee: './src/**/*.coffee',
-    staticContent: './src/**/!(*.coffee)'
+    sass: './src/**/*.scss',
+    staticContent: [
+        // Everything in src
+        './src/**/*',
+        // except
+        '!./src/**/*.coffee',
+        '!./src/**/*.scss'
+    ]
 };
 
 gulp.task('coffee', function() {
     gulp.src(paths.coffee)
         .pipe(coffee()).on('error', gutil.log)
+        .pipe(gulp.dest(paths.buildRoot));
+});
+
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+        .pipe(sass()).on('error', gutil.log)
         .pipe(gulp.dest(paths.buildRoot));
 });
 
@@ -26,7 +40,8 @@ gulp.task('clean', function() {
 
 gulp.task('watch', function() {
     gulp.watch(paths.coffee, ['coffee']);
+    gulp.watch(paths.sass, ['sass']);
     gulp.watch(paths.staticContent, ['static']);
 });
 
-gulp.task('default', ['clean', 'coffee', 'static']);
+gulp.task('default', ['clean', 'coffee', 'sass', 'static']);
