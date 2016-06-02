0fde9962904ee18fd74b2366029899c1d98984e2
Jingyu Yao <yjy.emails@gmail.com>
2016-06-02 00:57:02 -0400

    OOD + clean code for post list item

--- src/client/scripts/components/post-list-item.js
+++ src/client/scripts/components/post-list-item.js
@@ -7,19 +7,30 @@ class PostListItem extends React.Component {
     constructor(props) {
         super(props);
 
+        this.renderItemContents = this.renderItemContents.bind(this);
         this.postClicked = this.postClicked.bind(this);
     }
 
     render() {
         const post = this.props.post;
+        const title = (
+            <div className='mui--text-title title'>
+                {post.title}
+            </div>
+        );
+        const url = (
+            <span className='url'>
+                {post.url}
+            </span>
+        );
+
+        return this.renderItemContents(title, url);
+    }
+
+    renderItemContents(...contents) {
         return (
             <Panel onClick={this.postClicked} className='postListItem'>
-                <div className='mui--text-title title'>
-                    {post.title}
-                </div>
-                <span className='url'>
-                    {post.url}
-                </span>
+                {contents}
             </Panel>
         );
     }
