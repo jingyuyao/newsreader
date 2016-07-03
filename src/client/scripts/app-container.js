894483ac1618056a73c531362768c545851e2c2f
Jingyu Yao <yjy.emails@gmail.com>
2016-07-02 18:33:29 -0700

    Updated deps and added react-toolbox

--- src/client/scripts/app-container.js
+++ src/client/scripts/app-container.js
@@ -6,6 +6,4 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
-const appRef = ReactDOM.render(<App />, document.getElementById('appContainer'));
-
-export default appRef;
+ReactDOM.render(<App />, document.getElementById('appContainer'));
