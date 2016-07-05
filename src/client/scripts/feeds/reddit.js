210e3b0093a07b1ca2cd0c56f8945d582505d82e
Jingyu Yao <yjy.emails@gmail.com>
2016-07-05 14:09:41 -0700

    Refactored RedditFeed a bit so the functions look nicer

--- src/client/scripts/feeds/reddit.js
+++ src/client/scripts/feeds/reddit.js
@@ -12,25 +12,60 @@ export default class RedditFeed extends AbstractFeed {
         this.after = undefined;
     }
 
+    /**
+     * Override
+     */
     hasMore() {
         // TODO: heh
         return true;
     }
 
+    /**
+     * Override
+     */
     getMore() {
-        const url = this.baseUri.query({after: this.after}).toString();
-
-        return getJson(url)
-            .then(response => {
-                this.after = response.data.after;
-                return response.data.children.map(this.createPost);
-            })
+        return getJson(this.getUrl())
+            // Be careful about `this` not passing down into the handler
+            .then(this.handleResponse.bind(this))
             .catch(() => []);
     }
 
+    /**
+     * Handle the reponse from Reddit
+     * @param  {JsonResponse} response JSON reponse from Reddit's list api
+     * @return {List}          A list of Post
+     */
+    handleResponse(response) {
+        this.after = response.data.after;
+        return this.createPosts(response.data.children);
+    }
+
+    /**
+     * Get the url for the current page of posts
+     * @return {string} An url string
+     */
+    getUrl() {
+        return this.baseUri.query({after: this.after}).toString();
+    }
+
+    /**
+     * Create a list of Post from a list of things
+     * @param  {List} things A list of Reddit things
+     * @return {List}        A list of Post
+     */
+    createPosts(things) {
+        return things.map(this.createPost);
+    }
+
+    /**
+     * Create a post from a thing
+     * @param  {Thing} thing A Reddit thing
+     * @return {Post}        The Post this thing maps to
+     */
     createPost(thing) {
         const data = thing.data;
         // TODO: change primary view depending on thing's kind
+        // TODO: add secondary view by parsing the thing
         return new PostBuilder(data.id, data.title, Post.VIEWS.IFRAME)
             .iframe(data.url)
             .build();
